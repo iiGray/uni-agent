64b8f82233b9290152b4dee5a6fb07b13c02c04b
sora <iiiigray19@gmail.com>
2026-07-07 22:24:16 +0800

    update

--- examples/workflow/mem_agent/train_fully_async_32K_fsdp.sh
+++ examples/workflow/mem_agent/train_fully_async_32K_fsdp.sh
@@ -110,7 +110,7 @@ gpu_memory_utilization=0.6
 ray job submit --no-wait \
     --working-dir "${WORKING_DIR}" \
     -- python3 -m verl.experimental.fully_async_policy.fully_async_main \
-    --config-name='fully_async_ppo_megatron_trainer.yaml' \
+    --config-name='fully_async_ppo_trainer.yaml' \
     algorithm.adv_estimator=$adv_estimator \
     algorithm.use_kl_in_reward=$use_kl_in_reward \
     algorithm.kl_ctrl.kl_coef=$kl_coef \
@@ -125,14 +125,14 @@ ray job submit --no-wait \
     data.max_response_length=$max_response_length \
     data.filter_overlong_prompts=$filter_overlong_prompts \
     data.truncation='error' \
+    actor_rollout_ref.actor.strategy=fsdp \
+    actor_rollout_ref.ref.strategy=fsdp \
     actor_rollout_ref.model.path=$model_path \
     actor_rollout_ref.model.trust_remote_code=True \
     actor_rollout_ref.model.use_fused_kernels=$use_fused_kernels \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.optim.lr=$actor_lr \
-    actor_rollout_ref.actor.optim.lr_decay_style='constant' \
-    actor_rollout_ref.actor.optim.weight_decay=0.1 \
-    actor_rollout_ref.actor.optim.lr_decay_steps=$total_rollout_steps \
+    ++actor_rollout_ref.actor.optim.weight_decay=0.1 \
     actor_rollout_ref.actor.use_kl_loss=$use_kl_loss \
     actor_rollout_ref.actor.kl_loss_coef=$kl_loss_coef \
     actor_rollout_ref.actor.clip_ratio_low=$clip_ratio_low \
@@ -144,20 +144,6 @@ ray job submit --no-wait \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=$actor_max_token_len_per_gpu \
     actor_rollout_ref.actor.loss_agg_mode=$loss_agg_mode \
     actor_rollout_ref.actor.entropy_coeff=0 \
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=$TP_SIZE \
-    actor_rollout_ref.actor.megatron.context_parallel_size=$CP_SIZE \
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=$PP_SIZE \
-    actor_rollout_ref.actor.megatron.virtual_pipeline_model_parallel_size=$VPP_SIZE \
-    actor_rollout_ref.actor.megatron.expert_model_parallel_size=$EP_SIZE \
-    actor_rollout_ref.actor.megatron.expert_tensor_parallel_size=$ETP_SIZE \
-    actor_rollout_ref.actor.megatron.param_offload=$offload \
-    actor_rollout_ref.actor.megatron.grad_offload=$offload \
-    actor_rollout_ref.actor.megatron.optimizer_offload=$offload \
-    actor_rollout_ref.actor.megatron.use_mbridge=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.gradient_accumulation_fusion=False \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=2 \
     actor_rollout_ref.rollout.name=$rollout_name \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$infer_tp \
@@ -183,12 +169,6 @@ ray job submit --no-wait \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.hybrid_engine=False \
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=$log_prob_max_token_len_per_gpu \
-    actor_rollout_ref.ref.megatron.param_offload=$offload \
-    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=$TP_SIZE \
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=$PP_SIZE \
-    actor_rollout_ref.ref.megatron.context_parallel_size=$CP_SIZE \
-    actor_rollout_ref.ref.megatron.expert_model_parallel_size=$EP_SIZE \
-    actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=$ETP_SIZE \
     trainer.logger=['console','tensorboard'] \
     trainer.project_name=$project_name \
     trainer.experiment_name=$experiment_name \
--- examples/workflow/mem_agent/train_fully_async_32K_fsdp2.sh
+++ examples/workflow/mem_agent/train_fully_async_32K_fsdp2.sh
@@ -33,11 +33,11 @@ test_files="['$mem_test']"
 val_before_train=True
 
 # ================= ckpt =================
-model_name=Qwen2.5-3B-Instruct 
+model_name=Qwen3-4B
 
 # model_path=$DATA_ROOT/model/${model_name}
 
-model_path=/nvme1/hf_models/Qwen2.5-3B-Instruct 
+model_path=/nvme1/hf_models/Qwen3-4B
 #model_path=$DATA_ROOT/model/Qwen3-30B-A3B-Instruct-2507
 save_path=$DATA_ROOT/ckpts/$experiment_name
 
@@ -75,7 +75,7 @@ enable_chunked_prefill=True
 # ================= training =================
 offload=True
 TP_SIZE=1
-CP_SIZE=2
+CP_SIZE=4
 PP_SIZE=1
 VPP_SIZE=null
 EP_SIZE=1
@@ -97,12 +97,12 @@ partial_rollout=True
 # ================= inference =================
 rollout_name=vllm
 if [ "$rollout_name" = "vllm" ]; then
-    export VLLM_USE_V1=0
+    export VLLM_USE_V1=1
 fi
 infer_tp=4
 infer_dp=1
 infer_ep=1
-gpu_memory_utilization=0.6
+gpu_memory_utilization=0.4
 
 
 # ================= Main command =================
@@ -110,7 +110,7 @@ gpu_memory_utilization=0.6
 ray job submit --no-wait \
     --working-dir "${WORKING_DIR}" \
     -- python3 -m verl.experimental.fully_async_policy.fully_async_main \
-    --config-name='fully_async_ppo_megatron_trainer.yaml' \
+    --config-name='fully_async_ppo_trainer.yaml' \
     algorithm.adv_estimator=$adv_estimator \
     algorithm.use_kl_in_reward=$use_kl_in_reward \
     algorithm.kl_ctrl.kl_coef=$kl_coef \
@@ -125,14 +125,14 @@ ray job submit --no-wait \
     data.max_response_length=$max_response_length \
     data.filter_overlong_prompts=$filter_overlong_prompts \
     data.truncation='error' \
+    actor_rollout_ref.actor.strategy=fsdp \
+    actor_rollout_ref.ref.strategy=fsdp \
     actor_rollout_ref.model.path=$model_path \
     actor_rollout_ref.model.trust_remote_code=True \
     actor_rollout_ref.model.use_fused_kernels=$use_fused_kernels \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.optim.lr=$actor_lr \
-    actor_rollout_ref.actor.optim.lr_decay_style='constant' \
-    actor_rollout_ref.actor.optim.weight_decay=0.1 \
-    actor_rollout_ref.actor.optim.lr_decay_steps=$total_rollout_steps \
+    ++actor_rollout_ref.actor.optim.weight_decay=0.1 \
     actor_rollout_ref.actor.use_kl_loss=$use_kl_loss \
     actor_rollout_ref.actor.kl_loss_coef=$kl_loss_coef \
     actor_rollout_ref.actor.clip_ratio_low=$clip_ratio_low \
@@ -144,20 +144,6 @@ ray job submit --no-wait \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=$actor_max_token_len_per_gpu \
     actor_rollout_ref.actor.loss_agg_mode=$loss_agg_mode \
     actor_rollout_ref.actor.entropy_coeff=0 \
-    actor_rollout_ref.actor.megatron.tensor_model_parallel_size=$TP_SIZE \
-    actor_rollout_ref.actor.megatron.context_parallel_size=$CP_SIZE \
-    actor_rollout_ref.actor.megatron.pipeline_model_parallel_size=$PP_SIZE \
-    actor_rollout_ref.actor.megatron.virtual_pipeline_model_parallel_size=$VPP_SIZE \
-    actor_rollout_ref.actor.megatron.expert_model_parallel_size=$EP_SIZE \
-    actor_rollout_ref.actor.megatron.expert_tensor_parallel_size=$ETP_SIZE \
-    actor_rollout_ref.actor.megatron.param_offload=$offload \
-    actor_rollout_ref.actor.megatron.grad_offload=$offload \
-    actor_rollout_ref.actor.megatron.optimizer_offload=$offload \
-    actor_rollout_ref.actor.megatron.use_mbridge=True \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.gradient_accumulation_fusion=False \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_method=uniform \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_granularity=full \
-    +actor_rollout_ref.actor.megatron.override_transformer_config.recompute_num_layers=2 \
     actor_rollout_ref.rollout.name=$rollout_name \
     actor_rollout_ref.rollout.mode=async \
     actor_rollout_ref.rollout.tensor_model_parallel_size=$infer_tp \
@@ -183,16 +169,10 @@ ray job submit --no-wait \
     actor_rollout_ref.rollout.free_cache_engine=True \
     actor_rollout_ref.hybrid_engine=False \
     actor_rollout_ref.ref.log_prob_max_token_len_per_gpu=$log_prob_max_token_len_per_gpu \
-    actor_rollout_ref.ref.megatron.param_offload=$offload \
-    actor_rollout_ref.ref.megatron.tensor_model_parallel_size=$TP_SIZE \
-    actor_rollout_ref.ref.megatron.pipeline_model_parallel_size=$PP_SIZE \
-    actor_rollout_ref.ref.megatron.context_parallel_size=$CP_SIZE \
-    actor_rollout_ref.ref.megatron.expert_model_parallel_size=$EP_SIZE \
-    actor_rollout_ref.ref.megatron.expert_tensor_parallel_size=$ETP_SIZE \
     trainer.logger=['console','tensorboard'] \
     trainer.project_name=$project_name \
     trainer.experiment_name=$experiment_name \
-    trainer.n_gpus_per_node=2 \
+    trainer.n_gpus_per_node=4 \
     trainer.nnodes=$NNODES_TRAIN \
     trainer.val_before_train=$val_before_train \
     trainer.log_val_generations=$log_val_generations \
